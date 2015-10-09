8430d78bef9e17bb631c9714c2e07df9dbdf8794
michael <mdayan.comp@gmail.com>
2015-10-09 13:03:48 -0400

    webapp

--- webapp/app.py
+++ webapp/app.py
@@ -38,17 +38,41 @@ class StockForm(Form):
     submit = SubmitField('Submit')
 
 boom_data_all = pd.read_csv('../../nyc_collisions_data/longlat_filt.csv')
+boom_data_ped = pd.read_csv('../../nyc_collisions_data/longlat_filt_ped.csv')
+boom_data_bike = pd.read_csv('../../nyc_collisions_data/longlat_filt_bike.csv')
+manh_netw_data = pd.read_csv('../../nyc_collisions_data/nyc_intersections_MAN.csv')
+manhclean_netw_data = pd.read_csv('../../nyc_collisions_data/nyc_intersections_MAN_clean.csv')
+boro1_netw_data = pd.read_csv('../../nyc_collisions_data/nyc_intersections_boro1.csv')
 boom_data = boom_data_all
-#boom_data = boom_data_all.ix[:5000]
+#boom_data_all = boom_data_all.ix[:5000]
 #with open('js/boom100.json', 'w') as geojson_file:
     #geojson_file
     #geojson_file.write('collisions = [')
     #json.dump(geo_boom, geojson_file)
     #geojson_file.write('];')
-geo_boom = FeatureCollection([geojson.Feature(geometry=geojson.Point((p_long, p_lat)), properties={"NUMBER OF PERSONS INJURED": n_inj, 
+geo_boom_all = FeatureCollection([geojson.Feature(geometry=geojson.Point((p_long, p_lat)), properties={"NUMBER OF PERSONS INJURED": n_inj,
                                                                                  "NUMBER OF PERSONS KILLED": n_kill})
-            for p_long, p_lat, n_inj, n_kill in zip(boom_data['LONGITUDE'], boom_data['LATITUDE'], 
-                                                    boom_data['NUMBER OF PERSONS INJURED'], boom_data['NUMBER OF PERSONS KILLED'])])
+            for p_long, p_lat, n_inj, n_kill in zip(boom_data_all['LONGITUDE'], boom_data_all['LATITUDE'],
+                                                    boom_data_all['NUMBER OF PERSONS INJURED'], boom_data_all['NUMBER OF PERSONS KILLED'])])
+
+geo_boom_ped = FeatureCollection([geojson.Feature(geometry=geojson.Point((p_long, p_lat)), properties={"NUMBER OF PERSONS INJURED": n_inj,
+                                                                                 "NUMBER OF PERSONS KILLED": n_kill})
+            for p_long, p_lat, n_inj, n_kill in zip(boom_data_ped['LONGITUDE'], boom_data_ped['LATITUDE'],
+                                                    boom_data_ped['NUMBER OF PERSONS INJURED'], boom_data_ped['NUMBER OF PERSONS KILLED'])])
+
+geo_boom_bike = FeatureCollection([geojson.Feature(geometry=geojson.Point((p_long, p_lat)), properties={"NUMBER OF PERSONS INJURED": n_inj,
+                                                                                                        "NUMBER OF PERSONS KILLED": n_kill})
+            for p_long, p_lat, n_inj, n_kill in zip(boom_data_bike['LONGITUDE'], boom_data_bike['LATITUDE'],
+                                                    boom_data_bike['NUMBER OF PERSONS INJURED'], boom_data_bike['NUMBER OF PERSONS KILLED'])])
+
+geo_manh_netw = FeatureCollection([geojson.Feature(geometry=geojson.Point((p_long, p_lat)), properties={"Borough": 'Manhattan'})
+                                   for p_long, p_lat in zip(manh_netw_data['Lon'], manh_netw_data['Lat'])])
+
+geo_manhclean_netw = FeatureCollection([geojson.Feature(geometry=geojson.Point((p_long, p_lat)), properties={"Lon": p_long, "Lat": p_lat})
+                                        for p_long, p_lat in zip(manhclean_netw_data['Lon'], manhclean_netw_data['Lat'])])
+
+geo_boro1_netw = FeatureCollection([geojson.Feature(geometry=geojson.Point((p_long, p_lat)), properties={"Lon": p_long, "Lat": p_lat})
+                                        for p_long, p_lat in zip(boro1_netw_data['Lon'], boro1_netw_data['Lat'])])
 
 @app.route('/')
 def main():
@@ -62,9 +86,29 @@ def map():
 def send_js(path):
     return send_from_directory('js', path)
 
-@app.route("/geojson_data", methods=["GET", "POST"])
-def get_geojson_data():   
-    return geojson.dumps(geo_boom)
+@app.route("/geojson_all", methods=["GET", "POST"])
+def get_geojson_all():
+    return geojson.dumps(geo_boom_all)
+
+@app.route("/geojson_ped", methods=["GET", "POST"])
+def get_geojson_ped():
+    return geojson.dumps(geo_boom_ped)
+
+@app.route("/geojson_bike", methods=["GET", "POST"])
+def get_geojson_bike():
+    return geojson.dumps(geo_boom_bike)
+
+@app.route("/geojson_manh_netw", methods=["GET", "POST"])
+def get_geojson_manh():
+    return geojson.dumps(geo_manh_netw)
+
+@app.route("/geojson_cleanmanh_netw", methods=["GET", "POST"])
+def get_geojson_cleanmanh():
+    return geojson.dumps(geo_manhclean_netw)
+
+@app.route("/geojson_boro1_netw", methods=["GET", "POST"])
+def get_geojson_boro1():
+    return geojson.dumps(geo_boro1_netw)
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
